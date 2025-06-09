0acc96495abe9e6be818c1cf4299f06b16fc09b5
Sherafgan Khan <97927727+sherafyk@users.noreply.github.com>
2025-06-09 16:16:28 -0700

    Add WordPress-style collections

---
+++ collections/Pages.ts
@@ -0,0 +1,55 @@
+import { CollectionConfig } from 'payload/types';
+import { formatSlug } from '../utils/formatSlug';
+
+const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      unique: true,
+      required: true,
+    },
+    {
+      name: 'layout',
+      type: 'richText',
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: [
+        {
+          label: 'Draft',
+          value: 'draft',
+        },
+        {
+          label: 'Published',
+          value: 'published',
+        },
+      ],
+      defaultValue: 'draft',
+    },
+    {
+      name: 'publishedDate',
+      type: 'date',
+    },
+  ],
+  hooks: {
+    beforeValidate: [({ data }) => {
+      if (typeof data.title === 'string' && !data.slug) {
+        data.slug = formatSlug(data.title);
+      }
+      return data;
+    }],
+  },
+};
+
+export default Pages;
--- payload.config.ts
+++ payload.config.ts
@@ -1,10 +1,22 @@
 import { buildConfig } from 'payload/config';
 import Users from './collections/Users';
+import Media from './collections/Media';
+import Pages from './collections/Pages';
+import Posts from './collections/Posts';
+import Categories from './collections/Categories';
+import Tags from './collections/Tags';
 
 export default buildConfig({
   serverURL: process.env.SERVER_URL || 'http://localhost:3000',
   admin: {
     user: Users.slug,
   },
-  collections: [Users],
+  collections: [
+    Users,
+    Media,
+    Pages,
+    Posts,
+    Categories,
+    Tags,
+  ],
 });
